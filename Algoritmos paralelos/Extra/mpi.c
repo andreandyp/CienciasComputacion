78c03cc58c3612cfe9a4ea67aeb617fa8c2e5d27
André Michel Andy Pozos <andreandyp@outlook.com>
2018-12-11 19:33:26 -0600

    Listo MPI

--- Algoritmos paralelos/Extra/mpi.c
+++ Algoritmos paralelos/Extra/mpi.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <mpi.h>
 #include <time.h>
-#define N 4
 
 int main(int argc, char *argv[]){
 	srand(time(NULL));
@@ -12,46 +11,55 @@ int main(int argc, char *argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 	MPI_Comm_size(MPI_COMM_WORLD, &procesos);
 
-	int* matriz1 = malloc(N * N * sizeof(int));
-	int* matriz2t = malloc(N * N * sizeof(int));
+	int filA = 2;
+	int colA = 3;
+	int filB = 3;
+	int colB = 2;
+
+	int* matriz1 = malloc(filA * colA * sizeof(int));
+	int* matriz2t = malloc(filB * colB * sizeof(int));
 	int* matrizRes = NULL;
 	int* matrizFinal = NULL;
 
 	if(pid == 0){
-		matrizRes = malloc(N * sizeof(int));
-		matrizFinal = malloc(N * N * sizeof(int));
-		int* matriz2 = malloc(N * N * sizeof(int));
+		matrizRes = malloc(procesos * sizeof(int));
+		matrizFinal = malloc(filA * colB * sizeof(int));
+		int* matriz2 = malloc(filA * colB * sizeof(int));
 		
 		
-		for (int i = 0; i < N * N; ++i){
-	        matriz1[i] = rand()%2 + 1;
-	        matriz2[i] = rand()%2 + 1;
+		for (int i = 0; i < filA * colA; ++i){
+	        matriz1[i] = rand()%8 + 1;
+	    }
+
+	    for (int i = 0; i < filB * colB; ++i){
+	        matriz2[i] = rand()%8 + 1;
 	    }
 
 	    int k = 0;
-	    for(int i = 0; i < N; i++){
-	    	for (int j = 0; j < N * N; j += N){
+	    for(int i = 0; i < colB; i++){
+	    	for (int j = 0; j < filB * colB; j += colB){
 	    		matriz2t[k] = matriz2[i+j];
 	    		++k;
 	    	}
 	    }
 
-	    for (int i = 0; i < N * N; ++i){
+	    for (int i = 0; i < filA * colA; ++i){
 			printf("%d\t", matriz1[i]);
-			if((i+1)%N == 0){
+			if((i+1)%colA == 0){
 				printf("\n");
 			}
 	    }
 
 	    printf("-----\n");
 
-	    for (int i = 0; i < N * N; ++i){
+	    for (int i = 0; i < filB * colB; ++i){
 			printf("%d\t", matriz2[i]);
-			if((i+1)%N == 0){
+			if((i+1)%colB == 0){
 				printf("\n");
 			}
 			
 	    }
+	    printf("-----\n");
 
 	    free(matriz2);
 
@@ -59,74 +67,40 @@ int main(int argc, char *argv[]){
 
 	MPI_Barrier(MPI_COMM_WORLD);
 	
-    int* submatriz1 = malloc(N * sizeof(int));
-    if(N <= procesos){
-    	MPI_Scatter(matriz1, N, MPI_INT,
-				submatriz1, N, MPI_INT,
+    int* submatriz1 = NULL;
+    if(procesos >= filA){
+    	submatriz1 = malloc(procesos * sizeof(int));
+    	MPI_Scatter(matriz1, colA, MPI_INT,
+				submatriz1, colA, MPI_INT,
 				0, MPI_COMM_WORLD);	
-    }
-    else{
-    	int* matrizTemp = malloc(N*sizeof(int));
-    	if(pid == 0){
-    		for(int i = 0; i < N; i++){
-    			
-    			for(int j = N; j < N*N; j += N){
-    				for(int k = 0; k < N; k++){
-    					printf("...\n");
-    					matrizTemp[k] = matriz1[k+j];
-    					printf("__\n");
-    				}
-
-    			}
-				MPI_Send(
-				    matrizTemp,
-				    N,
-				    MPI_INT,
-				    i+1,
-				    555,
-				    MPI_COMM_WORLD);
-
-    		}
-    		for(int k = 0; k < N; k++){
-    			submatriz1[k] = matriz1[k];
-    		}
-
-    	}
-    	else{
-    		MPI_Status* hola;
-    		MPI_Recv(
-			    matrizTemp,
-			    N,
-			    MPI_INT,
-			    0,
-			    555,
-			    MPI_COMM_WORLD,
-			    hola);
-    		submatriz1 = matrizTemp;
-    	}
     }
 	
 
 	MPI_Bcast(
     	matriz2t,
-    	N*N,
+    	filB * colB,
     	MPI_INT,
     	0,
     	MPI_COMM_WORLD);
 
 	double inicio = MPI_Wtime();
-	for(int i = 0, l = 0; i < N * N; i+= N, ++l){
+
+	for(int i = 0, l = 0; i < filA * colB; i+= colA, ++l){
         int tmp = 0;
-        for(int j = 0; j < N; j++){
+        for(int j = 0; j < colA; j++){
+        	printf("[%d]%d x %d\n", pid, submatriz1[j], matriz2t[j+i]);
             tmp += matriz2t[j+i] * submatriz1[j];
         }
+
+        MPI_Barrier(MPI_COMM_WORLD);
         
         MPI_Gather(&tmp, 1, MPI_INT,
 		    matrizRes, 1, MPI_INT,
 		    0, MPI_COMM_WORLD);
 
         if(pid == 0){
-        	for(int k = 0, n = 0; k < N; k++, n += N){
+        	printf("[%d][%d][%d][%d]\n", matrizRes[0], matrizRes[1], matrizRes[2], matrizRes[3]);
+        	for(int k = 0, n = 0; k < filA; k++, n += filA){
         		matrizFinal[l+n] = matrizRes[k];
         	}
         }
@@ -142,9 +116,9 @@ int main(int argc, char *argv[]){
 
 	if(pid == 0){
 		printf("-----\n");
-		for (int i = 0; i < N * N; ++i){
+		for (int i = 0; i < filA * colB; ++i){
 			printf("%d\t", matrizFinal[i]);
-			if((i+1)%N == 0){
+			if((i+1)%colB == 0){
 				printf("\n");
 			}
 	    }
